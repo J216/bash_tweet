d8fde9df06d076dd00e327862366a27eadba433e
Jared <jaredhaer@gmail.com>
2018-09-05 12:45:56 -0500

    changed mv to cp for development purposes

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-sudo mv ./tweet /usr/bin/tweet
+sudo cp ./tweet /usr/bin/tweet
 sudo chmod +x /usr/bin/tweet
-sudo mv ./tweet.py /usr/bin/tweet.py
+sudo cp ./tweet.py /usr/bin/tweet.py
 
 python ./set-api-key.py
 
